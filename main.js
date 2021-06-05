521bc718c874090f39e767f4f3765988427ff50c
AdamOrielly <85327633+AdamOrielly@users.noreply.github.com>
2021-06-05 17:06:20 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ searchBtn.addEventListener('click', () => {
         console.log(res.status)
         console.log(res.statusText)
         console.log(res)
-        let Result = res.body.json()
+        let Result = JSON.stringify(res.json)
         let resResult = JSON.parse(Result)
         let duckResult = document.getElementById('duck__result')
         duckResult.style.display = 'flex'
