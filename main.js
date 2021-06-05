203bab191a9b63db323518fcbc85a2979c851675
AdamOrielly <85327633+AdamOrielly@users.noreply.github.com>
2021-06-05 13:20:40 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 const searchBtn = document.getElementById('form__button')
 searchBtn.addEventListener('click', () => {
     let formInputs = document.getElementById('input1').value
-    fetch('https://api.duckduckgo.com/?q='+formInputs+'&format=json', {
-        method: 'GET'
-    }).then(res => {
-        console.log(res.status)
-        console.log(res.statusText)
+    Headers.get('https://api.duckduckgo.com/?q='+formInputs+'&format=json').then(res => {
         console.log(res)
-        console.log(res.body)
-        let result = JSON.parse(res.body)
-    }).then(data => console.log(data)).catch(err => console.error('err'))
+        res.json()
+        console.log(res.json())
+    })
 })
